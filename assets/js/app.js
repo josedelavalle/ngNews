ac08b8a712584cdb96aafb9637dfbd7335289288
Jose DeLavalle <me@slickwebstudio.com>
2016-10-23 11:31:00 -0400

    Angular News API web app

---
+++ assets/js/app.js
@@ -0,0 +1,160 @@
+var app = angular.module('myApp', ['ngRoute','ngResource','ngAnimate','ngSanitize', 'ui.bootstrap']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  // $locationProvider.html5Mode(true);
+  $routeProvider
+   .when('/', {
+    templateUrl: 'main.html'
+    
+    // resolve: {
+    //   // I will cause a 1 second delay
+    //   delay: function($q, $timeout) {
+    //     var delay = $q.defer();
+    //     $timeout(delay.resolve, 1000);
+    //     return delay.promise;
+    //   }
+    // }
+  })
+  .when('/2', {
+    templateUrl: 'left-sidebar.html'
+   
+  })
+  .when('/3', {
+  	templateUrl: 'right-sidebar.html'
+  
+  })
+  .when('/4', {
+  	templateUrl: 'no-sidebar.html'
+  	
+  })
+  .otherwise({
+  	redirectTo: '/'
+  });
+}]);
+
+app.controller('appController', function($scope, $route, $http, $routeParams, $location, serviceName, factoryName) {
+     $scope.images = [];
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+     $scope.message = "News Source Picker";
+     $scope.submessage = "Powered by News API";
+     $scope.source="techcrunch";
+     $http.get('https://newsapi.org/v1/sources?language=en')
+
+      .success(function(data, status, headers, config) {
+       
+         $scope.news = data;
+         // console.log($scope.news.sources);
+      });
+
+     
+
+     $scope.goAPI = function() {
+      var thisSource;
+       thisSource = document.getElementById('newsSources');
+       //console.log(thisSource);
+       if (thisSource===null) {
+          thisSource='techcrunch';
+       } else {
+          thisSource=thisSource.value;
+       };
+       var thisURL = encodeURI("https://newsapi.org/v1/articles?source=" + thisSource + "&apiKey=fd3dd1dc190444ebbfce01a08c3c3760");
+          // console.log(thisURL);
+          $scope.newData = $http.get(thisURL)
+            .success(function(newData) {
+              
+              thisLength = newData.articles.length;
+              $scope.thisData = newData.articles;
+              console.log($scope.thisData);
+              for (i = 0; i < thisLength; i = i + 3) {
+                //console.log($scope.users[i].males);
+                
+                //console.log(newData.articles[i].title);
+                $scope.images[i] = newData.articles[i].urlToImage;
+                // console.log($scope.images[i]);
+
+              }
+              
+              //$scope.data.push(newData);
+            })
+            .error(function (error, status){
+              $scope.data.error = { message: error, status: status};
+              console.log($scope.data.error.status); 
+            }); 
+        };
+
+        $scope.goAPI();
+        $scope.goAPI2 = function goAPI2() {
+          alert('here');
+        };
+     $scope.pageClass = 'page-home';
+ });
+
+app.controller('leftController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+     console.log($scope.$route);
+     $scope.pageClass = 'page-left';
+ });
+app.controller('rightController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+     console.log($scope.$route);
+     $scope.pageClass = 'page-right';
+ });
+app.controller('noSidebarController', function($scope, $route, $routeParams, $location) {
+	 $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+     console.log($scope.$route);
+	 $scope.pageClass = 'page-none';
+});
+
+app.service('serviceName', ['$resource', function($resource){
+	return $resource(encodeURI('https://newsapi.org/v1/sources?language=en'));
+}]);
+app.factory('factoryName', function($http) {
+  return {
+      getSources: function() {
+        return  $http.get('https://newsapi.org/v1/sources?language=en');
+      }
+  };
+});
+
+app.directive('typeaheadFocus', function () {
+  return {
+    require: 'ngModel',
+    link: function (scope, element, attr, ngModel) {
+
+      //trigger the popup on 'click' because 'focus'
+      //is also triggered after the item selection
+      element.bind('click', function () {
+
+        var viewValue = ngModel.$viewValue;
+
+        //restore to null value so that the typeahead can detect a change
+        if (ngModel.$viewValue == ' ') {
+          ngModel.$setViewValue(null);
+        }
+
+        //force trigger the popup
+        ngModel.$setViewValue(' ');
+
+        //set the actual value in case there was already a value in the input
+        ngModel.$setViewValue(viewValue || ' ');
+      });
+
+      //compare function that treats the empty space as a match
+      scope.emptyOrMatch = function (actual, expected) {
+        if (expected == ' ') {
+          return true;
+        }
+        return actual.indexOf(expected) > -1;
+      };
+    }
+  };
+});
+
---
+++ assets/js/main.js
@@ -0,0 +1,137 @@
+/*
+	Verti by Pixelarity
+	pixelarity.com @pixelarity
+	License: pixelarity.com/license
+*/
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Dropdowns.
+			$('#nav > ul').dropotron({
+				mode: 'fade',
+				noOpenerFade: true,
+				speed: 300
+			});
+
+		// Off-Canvas Navigation.
+
+			
+
+			$('#work')
+				.scrollex({
+					top:		'0vh',
+					bottom:		'0vh',
+					delay:		50,
+					initialize:	function() {
+
+									var t = $(this);
+
+									t.find('.row.images')
+										.addClass('inactive');
+
+								},
+					terminate:	function() {
+
+									var t = $(this);
+
+									t.find('.row.images')
+										.removeClass('inactive');
+
+								},
+					enter:		function() {
+
+									var t = $(this),
+										rows = t.find('.row.images'),
+										length = rows.length,
+										n = 0;
+
+									rows.each(function() {
+										var row = $(this);
+										window.setTimeout(function() {
+											row.removeClass('inactive');
+										}, 100 * (length - n++));
+									});
+
+								},
+					leave:		function(t) {
+
+									var t = $(this),
+										rows = t.find('.row.images'),
+										length = rows.length,
+										n = 0;
+
+									rows.each(function() {
+										var row = $(this);
+										window.setTimeout(function() {
+											row.addClass('inactive');
+										}, 100 * (length - n++));
+									});
+
+								}
+				});
+			// Navigation Toggle.
+				$(
+					'<div id="navToggle">' +
+						'<a href="#navPanel" class="toggle"></a>' +
+					'</div>'
+				)
+					.appendTo($body);
+
+			// Navigation Panel.
+				$(
+					'<div id="navPanel">' +
+						'<nav>' +
+							$('#nav').navList() +
+						'</nav>' +
+					'</div>'
+				)
+					.appendTo($body)
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						hideOnSwipe: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'left',
+						target: $body,
+						visibleClass: 'navPanel-visible'
+					});
+
+			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+					$('#navToggle, #navPanel, #page-wrapper')
+						.css('transition', 'none');
+
+	});
+
+})(jQuery);
